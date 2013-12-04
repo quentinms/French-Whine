debb527df0eb669b491a5c920eaf998ee7ab30ba
quentinms <quentinmazarssimon@gmail.com>
2013-12-05 00:07:05 +0100

    Oublié une classe

---
+++ app/controllers/GreveItem.java
@@ -0,0 +1,10 @@
+package controllers;
+
+public class GreveItem{
+	public String greviste;
+	public String newsUrl;
+	public GreveItem(String greviste, String newsUrl){
+		this.greviste = greviste;
+		this.newsUrl = newsUrl;
+	}
+}
